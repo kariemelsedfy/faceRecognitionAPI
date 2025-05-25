fbfadf8d7e21c971dcd7534ac80e872c0cd98094
Karim ElSedfy <kariemelsedfy@gmail.com>
2025-05-25 11:47:24 +0300

    first commit

---
+++ api/register.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, APIRouter, File, UploadFile, Form
+from deepface import DeepFace
+import shutil
+import numpy
+router = APIRouter()
+
+#TODO: for now only using one image per user, for more accuracy do more.
+@router.post("/register")
+async def addFace(image : UploadFile = File(...), userID: int = Form(...)):
+    imagePath = f"tempImages/{userID}_temp.jpg"
+    with open(imagePath, "wb") as f:
+        shutil.copyfileobj(image.file, f)
+    
+    userEmbedding = DeepFace.represent(img_path=imagePath)
+    return 
+
+
